ac3f1b8ff66f51ebf2e4b51c677834e6234e8463
Toshiko <kunotoshiko17@gmail.com>
2020-10-27 13:17:12 +0100

    Initial commit

---
+++ src/Puzzle.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Toshiko Kuno
+ * Date: 2020-10-27
+ * Time: 13:11
+ * Project: IntelliJ IDEA
+ * Copyright: MIT
+ */
+
+
+public class Puzzle {
+
+
+    public Puzzle() {
+
+    }
+
+    public static void main(String[] args) {
+        Puzzle puzzle = new Puzzle();
+    }
+}
